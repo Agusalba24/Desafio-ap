1bb01599cee85dbe9c6e449140952ae1a1305401
Agustin.Albarracin <agusalba2019@gmail.com>
2024-04-26 17:55:39 -0300

    Desafio 1

---
+++ main.js
@@ -0,0 +1,8 @@
+let nombreProducto = prompt ('Zapatilla nike Af1')
+let precioUnitario = prompt ('180')
+let cantidadDeseada = prompt ('5')
+let precioFinal = parseInt(precioUnitario - cantidadDeseada)
+
+let monto = precioFinal + 900;
+
+alert(nombreProducto + 'En total sería: '+ monto)
